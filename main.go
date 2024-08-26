79a3fc4775a69067360e7f552c1e8ec7f55d3061
fancurson <fancurson@gmail.com>
2024-08-27 01:01:22 +0300

    The Signup Page - End

--- main.go
+++ main.go
@@ -18,6 +18,8 @@ func main() {
 		templates.FS, "signup.gohtml", "tailwind.gohtml"))
 	r.Get("/signup", usersC.New)
 
+	r.Post("/signup", usersC.Create)
+
 	r.Get("/", controllers.StaticHandler(
 		views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
 
@@ -26,9 +28,6 @@ func main() {
 
 	r.Get("/faq", controllers.FAQ(
 		views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
-
-	r.Get("/signup", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
