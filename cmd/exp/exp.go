26e311d9f468ed45a3903ca7db340ce3246e29f5
fancurson <fancurson@gmail.com>
2024-08-31 14:36:18 +0300

    exp file only

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,39 +1,77 @@
 package main
 
 import (
-	"html/template"
-	"os"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-type Widget struct {
-	Name  string
-	Price int
+func main() {
+	db, err := sql.Open("pgx", "host=localhost port=5432 user=baloo password=junglebook dbname=lenslocked sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("connected")
 }
 
-type ViewData struct {
-	Name    string
-	Widgets map[string]Widget
+/*
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+type config struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
+}
+
+func (cfg config) toString() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
 }
 
 func main() {
-
-	t, err := template.ParseFiles("hello.gohtml")
+	cfg := config{
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "baloo",
+		Password: "junglebook",
+		Database: "lenslocked",
+		SSLMode:  "disable",
+	}
+	db, err := sql.Open("pgx", cfg.toString())
 	if err != nil {
 		panic(err)
 	}
-
-	data := ViewData{
-		Name: "Vlad",
-		Widgets: map[string]Widget{
-			"a": {Name: "q", Price: 12},
-			"b": {Name: "w", Price: 13},
-			"c": {Name: "e", Price: 14},
-		},
-	}
-
-	err = t.Execute(os.Stdout, data)
+	defer db.Close()
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS users (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	email TEXT NOT NULL
+	);
+	CREATE TABLE IF NOT EXISTS orders (
+	id SERIAL PRIMARY KEY,
+	user_id INT NOT NULL,
+	amount INT,
+	description TEXT
+	);`)
 	if err != nil {
 		panic(err)
 	}
-
+	fmt.Println("Tables created.")
 }
+
+*/
